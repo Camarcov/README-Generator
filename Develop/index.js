1987bcd38dda4a10209aa2004aa5d79d74bdca98
Camarco <camarcovincent@gmail.com>
2024-05-01 17:06:31 -0400

    fixed file writing, added rough draft of readme template

--- Develop/index.js
+++ Develop/index.js
@@ -6,35 +6,58 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [{
     message: 'Whats the name of your project?',
-    name: 'projectName',
-},{
+    name: 'name',
+}, {
     message: 'Describe your project',
-    name: 'projDesc',
+    name: 'overview',
 },
 {
     message: 'Why did you make it?',
-    name: 'projInsp',
+    name: 'inspiration',
 },
 {
     message: 'How do we use it?',
-    name: 'projUse',
+    name: 'howToUse',
 },
 {
     message: 'Link your Github',
     name: 'Github',
-},{
+}, {
     message: 'Link to Project Repo',
     name: 'projRepo',
-},{
+}, {
     message: 'If applicable link to the Deployed page',
     name: 'projDep',
 },];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile('README.MD', data, (err) =>
+        err ? console.error(err) : console.log('Logged'))
+}
+
+const createREADME = (data) =>
+    `# ${data.name}
+    
+    ## Overview
+    ${data.overview}
+    ${data.inspiration}
+    
+    ## How to use
+    ${data.howToUse}
+
+    ## Links
+    ${data.Github}
+    ${data.projRepo}
+    ${data.projDep}`
+
 
 // TODO: Create a function to initialize app
-function init() {inquirer.prompt(questions)}
-
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        const README = createREADME(data)
+        writeToFile(README)
+    })
+}
 // Function call to initialize app
 init();
